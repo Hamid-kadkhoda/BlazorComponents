25ae8c415d782c95563d3f2b0b8ab5285b123e39
= <=>
2024-07-05 11:31:56 +0330

    feat: add icon library to project

---
+++ Blazor.Api/Modules/General/UserEndpoints.cs
@@ -0,0 +1,34 @@
+﻿using FastEndpoints;
+
+namespace Blazor.Api.Modules.General
+{
+    public class MyEndpoint : Endpoint<MyRequest, MyResponse>
+    {
+        public override void Configure()
+        {
+            Post("/hello/world");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(MyRequest r, CancellationToken c)
+        {
+            await SendAsync(new()
+            {
+                FullName = $"{r.FirstName} {r.LastName}",
+                Message = "Welcome to FastEndpoints..."
+            });
+        }
+    }
+
+    public class MyRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class MyResponse
+    {
+        public string FullName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ BlazorUIComponents/Components/Base/BlazorComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorUIComponents.Components.Base
+{
+    public partial class BlazorComponent: ComponentBase
+    {
+        [Parameter] public string? Style { get; set; }
+
+        [Parameter] public string? StyleClass { get; set; }
+    }
+}
